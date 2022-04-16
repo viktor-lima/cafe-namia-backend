3a5d911ba2ced844b3d03d89c6eb8eefdfec1ad2
viktor-lima <joaolimak10@outlook.com>
2022-04-16 18:18:11 -0300

    First commit

---
+++ src/main/java/com/vkl/cafemania/CafemaniaApplication.java
@@ -0,0 +1,13 @@
+package com.vkl.cafemania;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafemaniaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafemaniaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vkl/cafemania/CafemaniaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vkl.cafemania;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafemaniaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
