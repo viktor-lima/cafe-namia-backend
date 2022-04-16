3b8276cfdd7cc8a23f28ed0338e8ea076f7dd15b
viktor-lima <joaolimak10@outlook.com>
2022-04-16 18:38:09 -0300

    Test class Category

--- src/main/java/com/vkl/cafemania/resources/CategoryResource.java
+++ src/main/java/com/vkl/cafemania/resources/CategoryResource.java
@@ -1,16 +1,30 @@
 package com.vkl.cafemania.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.vkl.cafemania.domain.Category;
 
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryResource {
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String find() {
-		return "Rest ok";
+	public List<Category> find() {
+		Category category = new Category(1, "Doces");
+		Category category2 = new Category(1, "Salgados");
+		
+		List<Category> listCat = new ArrayList<>();
+		listCat.add(category);
+		listCat.add(category2);
+		
+		return listCat;
+		
+		
 	}
 	
 }
