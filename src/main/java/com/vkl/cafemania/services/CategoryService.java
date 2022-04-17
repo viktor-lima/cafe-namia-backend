356aeb6b2ee8c796eda0d9b3f2c24089314f1ed9
viktor-lima <joaolimak10@outlook.com>
2022-04-17 03:30:48 -0300

    refactoring on endpoint category find method

--- src/main/java/com/vkl/cafemania/services/CategoryService.java
+++ src/main/java/com/vkl/cafemania/services/CategoryService.java
@@ -2,11 +2,13 @@ package com.vkl.cafemania.services;
 
 import java.util.Optional;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.vkl.cafemania.domain.Category;
 import com.vkl.cafemania.repositories.CategoryRepository;
+import com.vkl.cafemania.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoryService{
@@ -16,8 +18,11 @@ public class CategoryService{
 	
 	public Category find(Integer id) {
 		Optional<Category> obj = repo.findById(id);
-		return obj.orElse(null);
+		
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Category.class.getName(), null));
 	}
+
 
 	public Category insert(Category obj) {
 		obj.setId(null);
