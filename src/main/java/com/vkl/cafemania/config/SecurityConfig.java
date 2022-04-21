35c1bc7b2ebbc7c4dab096176141ca12bfb7c850
viktor-lima <joaolimak10@outlook.com>
2022-04-21 15:14:57 -0300

    implementing authorization

--- src/main/java/com/vkl/cafemania/config/SecurityConfig.java
+++ src/main/java/com/vkl/cafemania/config/SecurityConfig.java
@@ -20,6 +20,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.vkl.cafemania.security.JWTAuthenticationFilter;
+import com.vkl.cafemania.security.JWTAuthorizationFilter;
 import com.vkl.cafemania.security.JWTUtil;
 
 @Configuration
@@ -56,6 +57,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 			.anyRequest().authenticated();
 		
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
